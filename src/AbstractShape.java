33a33ab445814dbf617be8b95acdc2cc1d8c22a7
Mark Goedegebure <Asus@192.168.2.14>
2015-10-01 09:24:44 +0200

    working on it...

---
+++ src/AbstractShape.java
@@ -0,0 +1,55 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+
+/**
+ * base class AbstractShape
+ *
+ */
+public abstract class AbstractShape extends Object
+
+{
+
+	// Data members
+	protected Point ptStart; // Start point of shape
+
+	protected Point ptEnd; // End point of shape
+
+	protected int nwidth; // Width of shape
+
+	protected int nheight; // Height of shape
+
+	protected Color clrFront; // Default color
+
+	public AbstractShape() {
+		// default color is red, try your combinations
+		clrFront = Color.red;
+	}
+
+	public abstract void setStart(Point pt);
+
+	public abstract void setEnd(Point pt);
+
+	public Point getStart() {
+		return ptStart;
+	}
+
+	public Point getEnd() {
+		return ptEnd;
+	}
+
+	public abstract void Draw(Graphics2D g);
+
+	public void setHeight(int nheight1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setWidth(int nwidth1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+// Base class cShape ends
